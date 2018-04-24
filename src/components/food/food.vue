86acfa89b56b6b03f904ec74009d0b53a19a5b9e
fcy9515 <fcy8915@126.com>
2018-04-24 16:03:43 +0800

    split组件

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -18,16 +18,22 @@
             <span class="now">￥{{food.price}}</span>
             <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
           </div>
-        </div>
-        <div class="cartcontrol-wrapper">
-          <cartcontrol :food="food"></cartcontrol>
-        </div>
-        <transition name="fade">
-          <div @click.stop.prevent="addFirst" class="buy"
-               v-show="!food.count || food.count===0">
-            加入购物车
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
           </div>
-        </transition>
+          <transition name="fade">
+            <div @click.stop.prevent="addFirst" class="buy"
+                 v-show="!food.count || food.count===0">
+              加入购物车
+            </div>
+          </transition>
+        </div>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
+        </div>
+        <split></split>
       </div>
     </div>
   </transition>
@@ -98,34 +104,47 @@
           text-decoration: line-through
           font-size: 10px
           color: rgb(147, 153, 159)
-    .cartcontrol-wrapper
-      position: absolute
-      right: 12px
-      bottom: 12px
-    .buy
-      position: absolute
-      right: 18px
-      bottom: 18px
-      z-index: 10
-      height: 24px
-      line-height: 24px
-      padding: 0 12px
-      box-sizing: border-box
-      border-radius: 12px
-      font-size: 10px
-      color: #fff
-      background: rgb(0, 160, 220)
-      opacity: 1
-      &.fade-enter-active, &.fade-leave-active
-        transition: all 0.2s
-      &.fade-enter, &.fade-leave-active
-        opacity: 0
-        z-index: -1
+      .cartcontrol-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        box-sizing: border-box
+        border-radius: 12px
+        font-size: 10px
+        color: #fff
+        background: rgb(0, 160, 220)
+        opacity: 1
+        &.fade-enter-active, &.fade-leave-active
+          transition: all 0.2s
+        &.fade-enter, &.fade-leave-active
+          opacity: 0
+          z-index: -1
+    .info
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: 14px
+        color: rgb(7, 17, 27)
+      .text
+        line-height: 24px
+        padding: 0 8px
+        font-size: 12px
+        color: rgb(77, 85, 93)
 </style>
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
   import Vue from 'vue';
   import cartcontrol from 'components/cartcontrol/index.vue';
+  import split from 'components/split/index.vue';
 
   export default {
     props: {
@@ -163,7 +182,8 @@
       }
     },
     components: {
-      cartcontrol
+      cartcontrol,
+      split
     }
   };
 </script>
